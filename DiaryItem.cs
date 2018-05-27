b4d2dbbc6ae807a362922500c9cb7d6b3147d06d
MockingT <31817173+MockingT@users.noreply.github.com>
2018-05-27 19:27:46 +0800

    Update DiaryItem.cs

--- DiaryItem.cs
+++ DiaryItem.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
 
 namespace MyDiary
 {
@@ -10,6 +11,8 @@ namespace MyDiary
     {
         public DateTime date { get; set; }
         public string description { get; set; }
-
+        public string text { get; set; }
+        public string image_src { get; set; }
+        private ImageSource ImgSource { get; set; }
     }
 }
