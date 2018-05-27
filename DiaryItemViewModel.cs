71d0138eacc1c12aa9bee3b6969749cc78949f65
MockingT <31817173+MockingT@users.noreply.github.com>
2018-05-27 19:28:19 +0800

    Update DiaryItemViewModel.cs

--- DiaryItemViewModel.cs
+++ DiaryItemViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,28 @@ namespace MyDiary
     public class DiaryItemViewModel
     {
         public ObservableCollection<DiaryItem> AllItems = new ObservableCollection<DiaryItem>();
-        public DiaryItemViewModel()
+        public static DiaryItemViewModel instance;
+        public static DiaryItemViewModel CreatInstance()
+        {
+            //是否已在内存中存在，不存在就实例化，然后返回此实例
+            if (instance == null)
+            {
+                instance = new DiaryItemViewModel();
+            }
+            return instance;
+        }
+        public void AddNewDiary()
+        {
+
+        }
+        public void DelteDiary()
+        {
+
+        }
+        /*public DiaryItemViewModel()
         {
             AllItems.Add(new DiaryItem());
             AllItems.Add(new DiaryItem());
-        }
+        }*/
     }
 }
