3d35dd57b01f52b00f5a5256283d7cf619490fda
penglsh <31811563+penglsh@users.noreply.github.com>
2018-06-01 20:09:40 +0800

    Update DiaryItemViewModel.cs

--- DiaryItemViewModel.cs
+++ DiaryItemViewModel.cs
@@ -11,6 +11,7 @@ namespace MyDiary
     public class DiaryItemViewModel
     {
         public ObservableCollection<DiaryItem> AllItems = new ObservableCollection<DiaryItem>();
+
         public static DiaryItemViewModel instance;
         public static DiaryItemViewModel CreatInstance()
         {
@@ -21,14 +22,40 @@ namespace MyDiary
             }
             return instance;
         }
-        public void AddNewDiary()
-        {
 
+        private DiaryItem selectedItem = default(DiaryItem);
+        public DiaryItem SelectedItem
+        {
+            get { return selectedItem; }
+            set { this.selectedItem = value; }
+        }
+
+        public void AddNewDiary(DateTimeOffset date, string des, string imgSrc, string record, string videoSrc)
+        {
+            this.AllItems.Add(new DiaryItem(date, des, imgSrc, record, videoSrc));
         }
         public void DelteDiary()
         {
+            this.AllItems.Remove(selectedItem);
 
+            this.selectedItem = null;
         }
+
+        public void UpDateDiary(string id, string des, string imgSrc,string record, string videoSrc)
+        {
+            this.selectedItem.id = id;
+            this.selectedItem.description = des;
+            this.selectedItem.image_src = imgSrc;
+            this.selectedItem.record = record;
+            this.selectedItem.videoSrc = videoSrc;
+        }
+
+        void ReadFromDB(DateTimeOffset date, string des, string imgSrc, string record, string videoSrc)
+        {
+            this.AllItems.Add(new DiaryItem(date, des, imgSrc, record, videoSrc));
+            this.selectedItem = null;
+        }
+        
         /*public DiaryItemViewModel()
         {
             AllItems.Add(new DiaryItem());
