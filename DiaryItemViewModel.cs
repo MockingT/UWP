9bfe818f41394eb8c1d7c8b7e0fc8d5144df8306
MockingT <31817173+MockingT@users.noreply.github.com>
2018-06-01 22:56:16 +0800

    Update DiaryItemViewModel.cs

--- DiaryItemViewModel.cs
+++ DiaryItemViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using SQLitePCL;
 
 namespace MyDiary
 {
@@ -41,7 +42,7 @@ namespace MyDiary
             this.selectedItem = null;
         }
 
-        public void UpDateDiary(string id, string des, string imgSrc,string record, string videoSrc)
+        public void UpDateDiary(string id, string des, string imgSrc, string record, string videoSrc)
         {
             this.selectedItem.id = id;
             this.selectedItem.description = des;
@@ -50,12 +51,19 @@ namespace MyDiary
             this.selectedItem.videoSrc = videoSrc;
         }
 
-        void ReadFromDB(DateTimeOffset date, string des, string imgSrc, string record, string videoSrc)
+        public void ReadFromDB()
         {
-            this.AllItems.Add(new DiaryItem(date, des, imgSrc, record, videoSrc));
-            this.selectedItem = null;
+            var db = App.conn;
+            using (var statement = db.Prepare(App.SQL_QUERY_VALUE))
+            {
+                while (SQLiteResult.ROW == statement.Step())
+                {
+                    DateTimeOffset _date = DateTimeOffset.Parse(statement[0].ToString());
+                    this.AllItems.Add(new DiaryItem(_date, statement[1].ToString(), statement[2].ToString(), statement[3].ToString(), statement[4].ToString()));
+                }
+            }
         }
-        
+
         /*public DiaryItemViewModel()
         {
             AllItems.Add(new DiaryItem());
