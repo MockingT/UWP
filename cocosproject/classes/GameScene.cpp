bc17eddb0eb7d263e2d0bc220ff7c95957b21112
MockingT <31817173+MockingT@users.noreply.github.com>
2018-07-21 16:55:30 +0800

    Update GameScene.cpp

--- cocosproject/classes/GameScene.cpp
+++ cocosproject/classes/GameScene.cpp
@@ -1,4 +1,4 @@
-﻿#include "GameScene.h"
+#include "GameScene.h"
 
 USING_NS_CC;
 
@@ -46,9 +46,54 @@ bool GameScene::init() {
 	ttfConfig.fontFilePath = "fonts/arial.ttf";
 	ttfConfig.fontSize = 24;
 
+	//the base
+	auto base = Sprite::create("bar.png");
+	base->setScaleX(3);
+	base->setPosition(Vec2(visibleSize.width / 2, 20));
+
+	auto baseBody = PhysicsBody::createBox(base->getContentSize(), PhysicsMaterial(100.0f, 0.5f, 0.0f));
+	baseBody->setCategoryBitmask(0xFFFFFFFF);
+	baseBody->setCollisionBitmask(0xFFFFFFFF);
+	baseBody->setContactTestBitmask(0xFFFFFFFF);
+	baseBody->setGravityEnable(false);
+	baseBody->setDynamic(false);
+	base->setPhysicsBody(baseBody);
+
+	this->addChild(base, 1);
+
+	auto base1 = Sprite::create("bar.png");
+	base1->setScaleY(0.5);
+	base1->setScaleX(0.7);
+	base1->setPosition(Vec2(visibleSize.width / 2 - 200, 150));
+
+	auto baseBody1 = PhysicsBody::createBox(base1->getContentSize(), PhysicsMaterial(100.0f, 0.5f, 0.0f));
+	baseBody1->setCategoryBitmask(0xFFFFFFFF);
+	baseBody1->setCollisionBitmask(0xFFFFFFFF);
+	baseBody1->setContactTestBitmask(0xFFFFFFFF);
+	baseBody1->setGravityEnable(false);
+	baseBody1->setDynamic(false);
+	baseBody1->setRotationEnable(false);
+	base1->setPhysicsBody(baseBody1);
+	this->addChild(base1, 1);
+
+	auto base2 = Sprite::create("bar.png");
+	base2->setScaleY(0.5);
+	base2->setScaleX(0.7);
+	base2->setPosition(Vec2(visibleSize.width / 2 + 300, 250));
+
+	auto baseBody2 = PhysicsBody::createBox(base2->getContentSize(), PhysicsMaterial(100.0f, 0.5f, 0.0f));
+	baseBody2->setCategoryBitmask(0xFFFFFFFF);
+	baseBody2->setCollisionBitmask(0xFFFFFFFF);
+	baseBody2->setContactTestBitmask(0xFFFFFFFF);
+	baseBody2->setGravityEnable(false);
+	baseBody2->setDynamic(false);
+	baseBody2->setRotationEnable(false);
+	base2->setPhysicsBody(baseBody2);
+	this->addChild(base2, 1);
+
 	auto playerLabel1 = Label::createWithTTF(ttfConfig, "player1: ");
 	playerLabel1->setPosition(Vec2(origin.x + 50, visibleSize.height - 15));
-	
+
 	auto playerLabel2 = Label::createWithTTF(ttfConfig, "player2: ");
 	playerLabel2->setPosition(Vec2(origin.x + 750, visibleSize.height - 15));
 	this->addChild(playerLabel1, 0);
@@ -86,29 +131,19 @@ bool GameScene::init() {
 	sp2->setPosition(Vec2(origin.x + 800 + pT2->getContentSize().width, origin.y + visibleSize.height - sp0->getContentSize().height));
 	addChild(sp2, 0);
 
-	/*player1 = Sprite::create("Fire 0.png");
-	player1->setPosition(visibleSize.width / 2 - 8, player1->getContentSize().height + 10);
-	this->addChild(player1, 1);
-	// 旋转180°
-	player1->setRotation(180);
-	headingLeft1 = false;
-
-	playerFeet = Sprite::create("Image 0.png");
-	playerFeet->setPosition(visibleSize.width / 2, playerFeet->getContentSize().height);
-	playerFeet->s
-	this->addChild(playerFeet, 1);*/
-
 	auto texture = Director::getInstance()->getTextureCache()->addImage("left.png");
 	auto frame0 = SpriteFrame::createWithTexture(texture, CC_RECT_PIXELS_TO_POINTS(Rect(0, 0, 60, 83)));
 	player1 = Sprite::createWithSpriteFrame(frame0);
 	player1->setPosition(Vec2(100, 100));
 	player1->setColor(Color3B::ORANGE);
 
-	auto playerBody = PhysicsBody::createBox(player1->getContentSize(), PhysicsMaterial(100.0f, 0.5f, 0.0f));
-	playerBody->setCategoryBitmask(0x00000001);
-	playerBody->setCollisionBitmask(0x00000001);
-	playerBody->setContactTestBitmask(0x00000001);
-	playerBody->setDynamic(false);
+	auto playerBody = PhysicsBody::createBox(player1->getContentSize(), PhysicsMaterial(100.0f, 1.0f, 0.5f));
+	playerBody->setCategoryBitmask(0x03);
+	playerBody->setCollisionBitmask(0x03);
+	playerBody->setContactTestBitmask(0x03);
+	playerBody->setGravityEnable(true);
+	playerBody->setDynamic(true);
+	playerBody->setRotationEnable(false);
 
 	player1->setPhysicsBody(playerBody);
 	player1->setFlippedX(true);
@@ -116,11 +151,13 @@ bool GameScene::init() {
 	player2 = Sprite::createWithSpriteFrame(frame0);
 	player2->setPosition(Vec2(visibleSize.width - 100, 100));
 
-	auto playerBody2 = PhysicsBody::createBox(player2->getContentSize(), PhysicsMaterial(100.0f, 0.5f, 0.0f));
-	playerBody2->setCategoryBitmask(0x00000001);
-	playerBody2->setCollisionBitmask(0x00000001);
-	playerBody2->setContactTestBitmask(0x00000001);
-	playerBody2->setDynamic(false);
+	auto playerBody2 = PhysicsBody::createBox(player2->getContentSize(), PhysicsMaterial(100.0f, 1.0f, 1.0f));
+	playerBody2->setCategoryBitmask(0x03);
+	playerBody2->setCollisionBitmask(0x03);
+	playerBody2->setContactTestBitmask(0x03);
+	playerBody2->setGravityEnable(true);
+	playerBody2->setRotationEnable(false);
+	playerBody2->setDynamic(true);
 
 	player2->setPhysicsBody(playerBody2);
 
@@ -137,7 +174,6 @@ bool GameScene::init() {
 
 	// 调度器
 	schedule(schedule_selector(GameScene::update), 0.04f, kRepeatForever, 0);
-
 	return true;
 }
 
@@ -145,7 +181,7 @@ void GameScene::moveAni() {
 	move.reserve(10);
 	char file[15];
 	for (int i = 0; i < 10; i++) {
-		sprintf(file, "Image %d.png",(i + 1) % 10);
+		sprintf(file, "Image %d.png", (i + 1) % 10);
 		auto frame = SpriteFrame::create(file, CC_RECT_PIXELS_TO_POINTS(Rect(0, 0, 33, 32)));
 		move.pushBack(frame);
 	}
@@ -173,7 +209,7 @@ void GameScene::fireAni() {
 
 //发射子弹
 void GameScene::fireBullet(int p) {
-	
+
 	MoveBy* moveby;
 	if (p == 1) {
 		auto bullet = Sprite::create("bullet.png");
@@ -223,7 +259,7 @@ void GameScene::fireBullet(int p) {
 			moveby = MoveBy::create(1.5f, Vec2(visibleSize.width, 0));
 		}
 		bullets2.push_back(bullet);
-		
+
 		addChild(bullet, 1);
 		//SimpleAudioEngine::getInstance()->playEffect("music/fire.wav", false);
 
@@ -234,12 +270,12 @@ void GameScene::fireBullet(int p) {
 			Sequence::create(
 				moveby,
 				CallFunc::create([bullet, this] {
-			if (bullet->getPosition().y >= visibleSize.height) {
-				auto tmp = bullet;
-				tmp->removeFromParentAndCleanup(true);
-				bullets2.remove(bullet);
-			}
-		}),
+					if (bullet->getPosition().y >= visibleSize.height) {
+						auto tmp = bullet;
+						tmp->removeFromParentAndCleanup(true);
+						bullets2.remove(bullet);
+					}
+				}),
 				nullptr
 			)
 		);
@@ -274,7 +310,7 @@ void GameScene::addKeyboardListener() {
 
 void GameScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
 	switch (code) {
-	
+
 	case EventKeyboard::KeyCode::KEY_CAPITAL_A:
 	case EventKeyboard::KeyCode::KEY_A:
 		movekey1 = 'A';
@@ -283,7 +319,7 @@ void GameScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
 			headingLeft1 = true;
 		}
 		if (player1->getPosition().x > 30) {
-			player1->getPhysicsBody()->setVelocity(Vec2(-500, 0));
+			player1->getPhysicsBody()->setVelocity(Vec2(-200,0) + player1->getPhysicsBody()->getVelocity());
 		}
 		player1->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
 		break;
@@ -295,7 +331,14 @@ void GameScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
 			headingLeft1 = false;
 		}
 		if (player1->getPosition().x < visibleSize.width - 30) {
-			player1->getPhysicsBody()->setVelocity(Vec2(500, 0));
+			player1->getPhysicsBody()->setVelocity(Vec2(200, 0) + player1->getPhysicsBody()->getVelocity());
+		}
+		player1->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
+		break;
+	case EventKeyboard::KeyCode::KEY_CAPITAL_W:
+	case EventKeyboard::KeyCode::KEY_W:
+		if (player1->getPosition().y < visibleSize.height - 100) {
+			player1->getPhysicsBody()->setVelocity(Vec2(0, 200) + player1->getPhysicsBody()->getVelocity());
 		}
 		player1->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
 		break;
@@ -306,7 +349,7 @@ void GameScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
 			headingLeft2 = true;
 		}
 		if (player2->getPosition().x > 30) {
-			player2->getPhysicsBody()->setVelocity(Vec2(-500, 0));
+			player2->getPhysicsBody()->setVelocity(Vec2(-200, 0) + player2->getPhysicsBody()->getVelocity());
 		}
 
 		player2->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
@@ -317,11 +360,16 @@ void GameScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
 			headingLeft2 = false;
 		}
 		if (player2->getPosition().x < visibleSize.width - 30) {
-			player2->getPhysicsBody()->setVelocity(Vec2(500, 0));
+			player2->getPhysicsBody()->setVelocity(Vec2(200, 0) + player2->getPhysicsBody()->getVelocity());
 		}
 		player2->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
 		break;
-	
+	case EventKeyboard::KeyCode::KEY_UP_ARROW:
+		if (player2->getPosition().y < visibleSize.height-100) {
+			player2->getPhysicsBody()->setVelocity(Vec2(0, 200) + player2->getPhysicsBody()->getVelocity());
+		}
+		player2->runAction(Animate::create(Animation::createWithSpriteFrames(move1, 0.05f, 1)));
+		break;
 	case EventKeyboard::KeyCode::KEY_CAPITAL_G:
 	case EventKeyboard::KeyCode::KEY_G:
 		fireBullet(1);
@@ -353,7 +401,7 @@ void GameScene::hitByBullet(EventCustom * event) {
 		if (player2->getBoundingBox().containsPoint(i->getPosition())) {
 			// player2 掉血
 			hp2 = hp2 - 10 <= 0 ? 0 : hp2 - 10;
-			
+
 			auto tmp = i;
 			bullets1.remove(i);
 			tmp->removeFromParentAndCleanup(true);
@@ -400,6 +448,12 @@ void GameScene::gameOver() {
 		auto gameOver = Sprite::create("gameOver.png");
 		gameOver->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
 		this->addChild(gameOver, 3);
+		auto label2 = Label::createWithTTF(ttfConfig, "Restart");
+		label2->setColor(Color3B(0, 0, 0));
+		auto replayBtn = MenuItemLabel::create(label2, CC_CALLBACK_1(GameScene::replayCallback, this));
+		Menu* replay = Menu::create(replayBtn, NULL);
+		replay->setPosition(visibleSize.width / 2, visibleSize.height / 2 - 200);
+		this->addChild(replay, 3);
 	}
 
 	if (hp1 <= 0) {
@@ -407,14 +461,19 @@ void GameScene::gameOver() {
 		winLabel->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2 - 100));
 		this->addChild(winLabel, 3);
 		Director::getInstance()->pause();
-		Director::getInstance()->getEventDispatcher()->removeAllEventListeners();
+		//Director::getInstance()->getEventDispatcher()->removeAllEventListeners();
 	}
 	else if (hp2 <= 0) {
 		auto winLabel = Label::createWithTTF(ttfConfig, "Player1 Win!");
 		winLabel->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2 - 100));
 		this->addChild(winLabel, 3);
 		Director::getInstance()->pause();
-		Director::getInstance()->getEventDispatcher()->removeAllEventListeners();
+		//Director::getInstance()->getEventDispatcher()->removeAllEventListeners();
 	}
 
+}
+
+void GameScene::replayCallback(Ref * pSender) {
+	Director::getInstance()->resume();
+	Director::getInstance()->replaceScene(GameScene::createScene());
 }
